b7f48f0990e36abc2e5e90c635a6aa419c7a71d8
amisalaam <amisalaam9@gmail.com>
2023-08-09 16:26:47 +0530

    backend hosting completed

--- advanzBackend/urls.py
+++ advanzBackend/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 
@@ -30,4 +31,7 @@ urlpatterns = [
        
 ]
 
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
