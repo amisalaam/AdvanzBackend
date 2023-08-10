0041e7e42e91cb133804a39ee89e19b1f9a72ddf
amisalaam <amisalaam9@gmail.com>
2023-08-10 12:35:52 +0530

    error 2

--- advanzBackend/settings.py
+++ advanzBackend/settings.py
@@ -103,7 +103,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("advanzbackend.onrender.com", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
